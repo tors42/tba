cb59afd87694e039e7ee55c97de9dc058e3df0c3
tors42 <4084220+tors42@users.noreply.github.com>
2025-03-26 19:04:58 +0100

    Use chariot-0.1.15

--- build.java
+++ build.java
@@ -26,7 +26,7 @@ void main(String[] args) throws Exception {
 
     if (! Files.exists(chariot))
         Files.copy(URI.create(
-                    "https://repo1.maven.org/maven2/io/github/tors42/chariot/0.1.14/chariot-0.1.14.jar"
+                    "https://repo1.maven.org/maven2/io/github/tors42/chariot/0.1.15/chariot-0.1.15.jar"
                     ).toURL().openStream(), chariot);
 
     List<String> modules = List.of(
